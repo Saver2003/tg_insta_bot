3ef220a84ed5eeceb5ca65f63a1d14f7cbe6b78a
Alex <saver2003@gmail.com>
2026-04-18 17:40:56 +0600

    #bot-1 инициализация бд

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
 // Entry point
 import './bot';
 import './server';
+
+import { Client } from "pg";
+import { initDB } from "./db/init";
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+async function start() {
+  await client.connect();
+
+  await initDB(client); // ← создаёт таблицы
+
+  console.log("DB ready");
+}
+
+start();
