81d4fab23ec516c5bc45d6fda62ce3b11beb40f7
Alex <saver2003@gmail.com>
2026-04-18 15:55:27 +0600

    #bot-1 минимально рабочий бот

--- src/bot.ts
+++ src/bot.ts
@@ -1,2 +1,14 @@
-// Telegraf initialization
-// ...будет реализовано позже
+import { Telegraf } from 'telegraf';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN!);
+
+bot.start((ctx) => {
+	ctx.reply('Привет! Я минималистичный Telegram-бот.');
+});
+
+bot.launch();
+// Экспортируем для использования в других частях приложения
+export default bot;
